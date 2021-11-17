80910bde187e9538e10aa95c0247e4743438f07a
fohin <fohinek99@gmail.com>
2021-11-17 20:52:54 +0100

    Change in the file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:pim_word_builder/function_button.dart';
 import 'package:pim_word_builder/home_screen.dart';
-import 'package:pim_word_builder/board.dart';
-import 'package:pim_word_builder/hand_letter.dart';
+import 'package:pim_word_builder/board_screen.dart';
 import 'package:pim_word_builder/app_colors.dart';
 
 void main() {
@@ -38,94 +36,6 @@ class App extends StatelessWidget {
                 ],
               )),
           body: const BoardScreen()),
-    );
-  }
-}
-
-class BoardScreen extends StatefulWidget {
-  const BoardScreen({Key? key}) : super(key: key);
-
-  @override
-  _BoardScreenState createState() => _BoardScreenState();
-}
-
-class _BoardScreenState extends State<BoardScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          // BOARD
-          Expanded(
-            flex: 8,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Container(
-                      color: AppColors.misty,
-                      child: const Padding(
-                        padding: EdgeInsets.all(20.0),
-                        child: Center(child: Board()),
-                      )),
-                ),
-              ],
-            ),
-          ),
-          // HAND
-          Expanded(
-            flex: 1,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Container(
-                    color: AppColors.misty,
-                    child: Row(
-                      children: [
-                        HandLetter(),
-                        HandLetter(),
-                        HandLetter(),
-                        HandLetter(),
-                        HandLetter(),
-                        HandLetter(),
-                        HandLetter(),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // BUTTONS
-          Expanded(
-            flex: 1,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  child: Container(
-                    color: AppColors.misty,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: const [
-                        FunctionButton(icon: Icons.undo_rounded, text: 'UNDO'),
-                        FunctionButton(
-                            icon: Icons.change_circle_rounded,
-                            text: 'EXCHANGE'),
-                        FunctionButton(
-                            icon: Icons.shuffle_on_rounded, text: 'SHUFFLE'),
-                        FunctionButton(
-                            icon: Icons.skip_next_rounded, text: 'END TURN'),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
